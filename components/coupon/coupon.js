3950b9166e85cae0761d3d47d0dcbf16d6449cd2
1314mxc <1562494673@qq.com>
2020-08-26 14:56:48 +0800

    新增coupon组件

---
+++ components/coupon/coupon.js
@@ -0,0 +1,27 @@
+// components/coupon/coupon.js
+Component({
+
+  options:{
+    multipleSlots:true
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
